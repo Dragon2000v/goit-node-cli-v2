2423d55de26f7503f826b710232b688bf9f26143
Dragon2000v <shmatko064@gmail.com>
2024-06-24 16:40:16 +0300

    add getAllContacts and updateContacts

---
+++ src/scripts/contacts/updateContacts.js
@@ -0,0 +1,6 @@
+import fs from 'node:fs/promises';
+
+import { PATH_DB } from '../../constants/contacts.js';
+
+export const updateContacts = (contactList) =>
+  fs.writeFile(PATH_DB, JSON.stringify(contactList, null, 2));
