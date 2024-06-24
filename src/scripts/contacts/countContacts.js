5cc84113b9d25190adcc22f040462c0d7911637a
Dragon2000v <shmatko064@gmail.com>
2024-06-24 16:42:31 +0300

    add countContacts

---
+++ src/scripts/contacts/countContacts.js
@@ -0,0 +1,8 @@
+import { getAllContacts } from './getAllContacts.js';
+
+export const countContacts = async () => {
+  const contactsList = await getAllContacts();
+  return contactsList.length;
+};
+
+console.log(await countContacts());
